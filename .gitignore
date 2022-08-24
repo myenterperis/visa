d3bab090d9e951b4cca9ac4ce4042dd28a20dd19
yaya musiliu <musiyaya12@gmail.com>
2022-08-23 23:39:07 -0400

    .gitignore file modifiedd

---
+++ .gitignore
@@ -0,0 +1,4 @@
+# Deployiny a python web application
+python ('hello world')
+#= python script
+deploy in the dev env
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-# Deployiny a python web application
-python ('hello world')
-#= python script
-deploy in the dev env
